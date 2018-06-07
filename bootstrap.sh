5b3d348ccf27ee48337a884f71273f06c5777cc0
Rick Sherman <rick@shermdog.com>
2018-06-07 16:55:12 -0500

    2018.1.1 and increase memory

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,13 +8,13 @@ echo "Setting hostname..."
 /usr/bin/hostname -F /etc/hostname
 
 echo "Downloading Puppet Enterprise..."
-/usr/bin/curl -O https://s3.amazonaws.com/pe-builds/released/2018.1.0/puppet-enterprise-2018.1.0-el-7-x86_64.tar.gz
+/usr/bin/curl -O https://s3.amazonaws.com/pe-builds/released/2018.1.1/puppet-enterprise-2018.1.1-el-7-x86_64.tar.gz
 
 echo "Extracting Puppet Enterprise..."
-/usr/bin/tar -xvf puppet-enterprise-2018.1.0-el-7-x86_64.tar.gz
+/usr/bin/tar -xvf puppet-enterprise-2018.1.1-el-7-x86_64.tar.gz
 
 echo "Installing Puppet Enterprise..."
-./puppet-enterprise-2018.1.0-el-7-x86_64/puppet-enterprise-installer -c demo-pe.conf
+./puppet-enterprise-2018.1.1-el-7-x86_64/puppet-enterprise-installer -c demo-pe.conf
 
 # Puppet Enterprise installation is completed by subsequent agent runs
 echo "Running puppet..."
@@ -27,6 +27,9 @@ echo "Installing modules from Puppetfile"
 
 echo "Use puppet apply for further bootstrap..."
 /usr/local/bin/puppet apply devnet.pp
+
+echo "Prevent puppet agent from automatically running..."
+/usr/local/bin/puppet config set runinterval 1y
 
 # sync up all the new modules
 echo "Running puppet again..."
